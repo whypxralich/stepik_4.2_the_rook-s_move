7b405fd81ceba3f9ab3c5a2ae338a4c2b882744f
whypxralich <whypxrlch@gmail.com>
2023-12-18 01:20:59 +0300

    Create stepik_4.2_the_rook's_move.py

---
+++ stepik_4.2_the_rook's_move.py
@@ -0,0 +1,11 @@
+#stepik_4.2_the_rook's_move
+
+st1 = int(input())
+gr1 = int(input())
+st2 = int(input())
+gr2 = int(input())
+
+if st1 == st2 or gr1 == gr2:
+    print ("YES")
+else:
+    print ("NO")
